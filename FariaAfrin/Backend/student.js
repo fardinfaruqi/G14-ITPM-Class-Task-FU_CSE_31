cff98f84ff91b6d47cc5a7997c3bc8f48c08e894
fariaisha <150256086+fariaisha@users.noreply.github.com>
2026-05-12 20:59:32 +0600

    Add files via upload
    backend task

---
+++ FariaAfrin/Backend/student.js
@@ -0,0 +1,37 @@
+const students = [
+  {
+    name: "Faria Afrin ",
+    id: "232031016",
+    batch: "31st",
+    program: "BSc in CSE",
+    department: "Computer Science & Engineering",
+    university: "Feni University",
+    semester: "6th",
+    email: "232031016@feniuniversity.ac.bd",
+    cgpa: 3.95,
+  },
+  {
+    name: "Md Rakibun Ateed Ramim",
+    id: "232031015",
+    batch: "31st",
+    program: "BSc in CSE",
+    department: "Computer Science & Engineering",
+    university: "Feni University",
+    semester: "6th",
+    email: "232031015@feniuniversity.ac.bd",
+    cgpa: 3.75,
+  },
+  {
+    name: "Rakibul Islam",
+    id: "232031043",
+    batch: "31st",
+    program: "BSc in CSE",
+    department: "Computer Science & Engineering",
+    university: "Feni University",
+    semester: "6th",
+    email: "232031043@feniuniversity.ac.bd",
+    cgpa: 3.88,
+  },
+];
+
+module.exports = students;
